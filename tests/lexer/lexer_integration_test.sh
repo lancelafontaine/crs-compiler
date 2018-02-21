292744bce0a54c3dac124205ba7807f1e1ac3482
Lance Lafontaine <lancelafontaine@protonmail.com>
2018-02-20 21:29:59 -0500

    Provides a wide variety of integration tests for the accessor, array, inheritance and scope resolution operator tokenization during lexical analysis

--- tests/lexer/lexer_integration_test.sh
+++ tests/lexer/lexer_integration_test.sh
@@ -70,6 +70,18 @@ SOURCE_FILE="${SCRIPTPATH}/source_files/keyword_bool.txt"
 EXPECTED_OUTPUT="Token { class: '< keyword >', lexeme: 'bool' }"
 run_test
 
+SOURCE_FILE="${SCRIPTPATH}/source_files/integer_invalid_with_letters.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/integer_invalid_with_(.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/integer_invalid_with_[.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
 SOURCE_FILE="${SCRIPTPATH}/source_files/integer_invalid_0123.txt"
 EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
 run_test
@@ -83,6 +95,18 @@ EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial s
 run_test
 
 SOURCE_FILE="${SCRIPTPATH}/source_files/float_invalid_12.34e01.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/float_invalid_with_(.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/float_invalid_with_[.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/float_invalid_with_letters.txt"
 EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
 run_test
 
@@ -232,6 +256,130 @@ run_test
 
 SOURCE_FILE="${SCRIPTPATH}/path/does/not/exist.txt"
 EXPECTED_OUTPUT="The input file path provided does not exist on the filesystem."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/accessor_operator_valid.txt"
+EXPECTED_OUTPUT="Token { class: '< accessor_operator, . >', lexeme: '.' }"
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/accessor_operator_valid_after_identifier.txt"
+EXPECTED_OUTPUT="Token { class: '< accessor_operator, . >', lexeme: '.' }"
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/accessor_operator_valid_after_array.txt"
+EXPECTED_OUTPUT="Token { class: '< accessor_operator, . >', lexeme: '.' }"
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/open_square_bracket.txt"
+EXPECTED_OUTPUT="Token { class: '< open_square_bracket >', lexeme: '\[' }"
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/close_square_bracket.txt"
+EXPECTED_OUTPUT="Token { class: '< close_square_bracket >', lexeme: '\]' }"
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_valid.txt"
+EXPECTED_OUTPUT="Token { class: '< inheritance_operator >', lexeme: ':' }"
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_valid_class_with_spaces.txt"
+EXPECTED_OUTPUT="Token { class: '< inheritance_operator >', lexeme: ':' }"
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_valid_class_without_spaces.txt"
+EXPECTED_OUTPUT="Token { class: '< inheritance_operator >', lexeme: ':' }"
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_valid_with_comment.txt"
+EXPECTED_OUTPUT="Token { class: '< inheritance_operator >', lexeme: ':' }"
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_valid_with_newline.txt"
+EXPECTED_OUTPUT="Token { class: '< inheritance_operator >', lexeme: ':' }"
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_open_parens.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_close_parens.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_open_curly_brace.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_close_curly_brace.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_open_square_bracket.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_close_square_bracket.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_+.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_-.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_*.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_%.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_semicolon.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_=.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_<.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_>.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_&.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_|.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_!.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_!.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/inheritance_operator_invalid_with_accessor_operator.txt"
+EXPECTED_OUTPUT="Invalid syntax: error state reached. Resetting FSM to initial state."
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/scope_resolution_operator_valid.txt"
+EXPECTED_OUTPUT="Token { class: '< scope_resolution_operator >', lexeme: '::' }"
+run_test
+
+SOURCE_FILE="${SCRIPTPATH}/source_files/scope_resolution_operator_valid_with_namespace.txt"
+EXPECTED_OUTPUT="Token { class: '< scope_resolution_operator >', lexeme: '::' }"
 run_test
 
 echo "Number of tests: $NUM_TESTS"
